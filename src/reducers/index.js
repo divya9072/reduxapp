f2ae89550570bfba44e00a7841f7b7d58972385d
divya9072 <divi9072@gmail.com>
2022-02-03 19:19:46 +0530

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import changeTheNumber from "./addsub";
+
+import { combineReducers } from "redux";
+
+const reducers = combineReducers(
+    {
+        changeTheNumber
+    }
+);
+
+export default reducers;
